544d140844263ecc10bacf162607e7d2564dd22e
sathyadevops <sathyadevops1@gmail.com>
2019-06-20 03:24:03 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,13 @@
+
+  class Demo
+  {
+     p s void main(....)
+     {
+        s.o.print(" Good day ");
+        s.o.print(" Good day ");
+        s.o.print(" Good day ");
+        s.o.print(" Good day ");
+        s.o.print(" Good day ");
+
+     }
+  }
